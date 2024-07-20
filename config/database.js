9ec8bb887e0be63eabc6e827564d52c6f7ba14ca
ngbaduyy10 <ngbaduyy05@gmail.com>
2024-07-20 08:48:07 +0700

    client product page fixed

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports.connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(process.env.MONGO_URL);
         console.log('Connect successfully');
     } catch (error) {
         console.log('Connect failure');
